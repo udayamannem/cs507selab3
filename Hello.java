1e614ee91b565af7603a594fdc9b6ce664802daa
udayamannem <mannemu132@strose.edu>
2015-10-04 00:54:40 -0400

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,9 @@
+/* Hello.java*/
 public class Hello
 {
 Public static void main( String args[]) {
 System.out.println("Udaya");
 System.out.println("Group members: Keerthika, Weijian , Mohammed");
+System.out.println("This is SE lab3 java program");
 }
 }
