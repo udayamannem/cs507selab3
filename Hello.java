152335f3ed117b7cf3483b893e6fcca5f8820641
udayamannem <mannemu132@strose.edu>
2015-10-04 00:13:59 -0400

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
-class Hello
+public class Hello
 {
 Public static void main( String args[]) {
 System.out.println("Udaya");
+System.out.println("Group members: Keerthika, Weijian , Mohammed");
 }
 }
