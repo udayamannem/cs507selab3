d460123b96fb0c05ac37c6b47d1784cc4fe66bae
udayamannem <mannemu132@mogul.strose.edu>
2015-10-05 12:18:05 -0400

    this is commited program after modifed.

--- Hello.java
+++ Hello.java
@@ -5,5 +5,6 @@ Public static void main( String args[]) {
 System.out.println("Udaya");
 System.out.println("Group members: Keerthika, Weijian , Mohammed");
 System.out.println("This is SE lab3 java program");
+System.out.println("the edited version is this ");
 }
 }
