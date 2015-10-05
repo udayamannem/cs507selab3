3911bd19761eee02fa948b816c7bea511da8e71c
williamduwillwin <williamduwillwin@gmail.com>
2015-10-05 14:07:21 -0400

    this is edited by weijian

--- Hello.java
+++ Hello.java
@@ -7,6 +7,7 @@ System.out.println("Group members: Udaya,Keerthika, Weijian , Mohammed");
 System.out.println("This is SE lab3 java program");
 System.out.println("the edited version is this by Udaya ");
 System.out.println("the edited version is this by Keerthika ");
-System.out.println("This edited version is by Mohammed")
+System.out.println("This edited version is by Mohammed"):
+System.out.println("This is edidted verison is by Weijian");
 }
 }
