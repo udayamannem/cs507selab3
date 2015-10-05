be970a63715ae2847fcf3fcefa13a22b5f561620
udayamannem <mannemu132@mogul.strose.edu>
2015-10-05 13:42:10 -0400

    this is by udaya

--- Hello.java
+++ Hello.java
@@ -3,8 +3,8 @@ public class Hello
 {
 Public static void main( String args[]) {
 System.out.println("Udaya");
-System.out.println("Group members: Keerthika, Weijian , Mohammed");
+System.out.println("Group members: Udaya,Keerthika, Weijian , Mohammed");
 System.out.println("This is SE lab3 java program");
-System.out.println("the edited version is this ");
+System.out.println("the edited version is this by Udaya ");
 }
 }
